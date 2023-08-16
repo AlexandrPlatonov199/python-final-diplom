b44efdb93de23b38571aca15f02a37b42dee6fbb
Alexander Platonov <sasa20sasha19910@yandex.ru>
2023-08-16 12:49:38 +0300

    update urls

--- orders/orders/urls.py
+++ orders/orders/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('users_auth.urls')),
+    path('api/v1/user/', include('users_auth.urls')),
     path('api/v1/', include('create_orders.urls')),
+    path('api/v1/shop/', include('ordering_goods.urls'))
 ]
