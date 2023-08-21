5aa88812a37d2c817f5e7706a92bd7080aa5e253
Alexander Platonov <sasa20sasha19910@yandex.ru>
2023-08-21 18:28:48 +0300

    create custom pipeline for social auth

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -201,6 +201,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
+    'users_auth.pipeline.social_auth_login',
 )
 
 SPECTACULAR_SETTINGS = {
